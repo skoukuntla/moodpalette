35321aa1ee04758ccdc8a75bec58b9c3d4c3ba0f
riyaverma11 <verma108@purdue.edu>
2023-02-06 19:31:42 -0500

    added CRUD (create,read, update, delete) API calls for users

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,57 @@
-const router = require("express").Router()
-module.exports = router
+const User = require("../models/User");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-router.get("/",(req,res)=>{
-    res.send("hey it's user")
- })
+//params --> what is in the url
+//body --> what is sent in the request
 
+//update user
+router.put("/:userId", async (req, res) => {
+    if (req.body.userId === req.params.userId) {
+      if (req.body.password) {
+        try {
+          const salt = await bcrypt.genSalt(10);
+          req.body.password = await bcrypt.hash(req.body.password, salt);
+        } catch (err) {
+          return res.status(500).json(err);
+        }
+      }
+      try {
+        const user = await User.findByIdAndUpdate(req.params.userId, {
+          $set: req.body,
+        });
+        res.status(200).json("Account has been updated");
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    } else {
+      return res.status(403).json("You can update only your account!");
+    }
+  });
+  
+  //delete user
+  router.delete("/:userId", async (req, res) => {
+    if (req.body.userId === req.params.userId) {
+      try {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json("Account has been deleted");
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    } else {
+      return res.status(403).json("You can delete only your account!");
+    }
+  });
+  
+  //get a user
+  router.get("/:userId", async (req, res) => {
+    try {
+      const user = await User.findById(req.params.userId);
+      const { password, updatedAt, ...other } = user._doc;
+      res.status(200).json(other);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+ module.exports = router
