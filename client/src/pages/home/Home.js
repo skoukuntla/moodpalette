3f52a0b0453114910712bcfd1c9d927be58cfccd
riyaverma11 <verma108@purdue.edu>
2023-02-08 14:06:13 -0500

    created folders for front end components and made a homepage

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,11 @@
+//HOME PAGE FRONTEND CODE
+import React from "react";
+import "./home.css"
+
+export default function Home() {
+    return (
+        <div>
+            this is home page
+        </div>
+    )
+}
