f47410b8e1e3198ec96dfafcdfb76a49ea8dbea8
DarshanaVenkatesh <venkat82@purdue.edu>
2023-02-09 01:05:09 -0500

    added react-router-dom and link routing to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import Register from "./pages/register/Register"
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
-  return <Register/>
 
+	return (
+		<Router>
+			<Routes>
+
+      <Route path="/" element={<Register />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+
+			</Routes>
+		</Router>
+
+
+
+	);
 }
 
 export default App;
