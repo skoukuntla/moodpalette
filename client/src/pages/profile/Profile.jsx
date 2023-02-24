0c2eab18e4643b813abfaf074f5472361ea65edc
ss-mathew <70663265+ss-mathew@users.noreply.github.com>
2023-02-24 15:55:10 -0500

    Profile Email form, notification button merge

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -8,7 +8,29 @@ import { useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import emailjs from 'emailjs-com';
+
+const notify = () => {
+  toast("Make sure to fill out your Mood Palette for the day!");
+}
+
 export default function Profile() {
+
+  const form = useRef();
+    function sendEmail(e) {
+        e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+    
+        emailjs.sendForm('service_t3oalh7', 'template_zh4imic', e.target, 'QDfiNV70JdveqlrKq')
+          .then((result) => {
+              window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
+
   const { user, dispatch } = useContext(AuthContext);
   console.log(user)
 
@@ -173,6 +195,37 @@ export default function Profile() {
           </div>{" "}
           {/* this div has the two buttons --> leads to popups*/}
         </div>
+        <div className="email">
+            <div classname="emailWrapper">
+            <span className="loginDesc">
+            Subscribe to email notifications!
+          </span>
+            <br/>
+            <div className="emailRight">
+            <form className="contact-form" onSubmit={sendEmail}>
+                <input type="hidden" name="contact_number" />
+                <label>Name</label>
+                <input className="from_name" type="text" name="from_name" />
+                <br/>
+                <label>Email</label>
+                <input className="from_email" type="email" name="from_email" />
+                <br/>
+                <label>Time</label>
+                <input className="from_time" type="time" name="from_time" />
+                <br/>
+                <label>Subscribe</label>
+                <input  type="checkbox" name="from_checkbox"/>
+                <br/>
+                <input className="submit" type="submit" value="Send" />
+                <br/>
+                <button className="notify" onClick={notify}>Notify!</button>
+                </form>
+
+            </div>
+            <ToastContainer/>
+            </div>
+        </div>
     </>
+    
   );
 }
