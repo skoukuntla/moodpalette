fb9ea7b0cc6425a02185f200a3f480b20e74451a
Aayushi Neema <neema.aayushi@gmail.com>
2023-03-22 11:19:28 -0400

    axior error in here

--- client/src/pages/home/spotify/spotify.js
+++ client/src/pages/home/spotify/spotify.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { AuthContext } from "../../../context/AuthContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Buffer } from 'buffer';
 
 const CLIENT_ID = "1f57088263ff49bebe219245a8e8c6c9"
 const CLIENT_SECRET = "c26a902aef59405684bd3fd3c7a372c9"
@@ -10,49 +12,62 @@ const SCOPES = [] // Spotify account info that we want to have access to
 const SCOPES_URL = SCOPES.join("%20");
 
 function Spotify() {
-    const { user } = useContext(AuthContext);
-    const handleLogin = async () => {
-        // redirect to Spotify authorization page
-        window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES_URL}&response_type=code&show_dialog=true`;
+    const { user, dispatch } = useContext(AuthContext);
+    const navigate = useNavigate();
+    useEffect(() => {
+        async function fetchTokens() {
+            if (window.location.search) {
+                const afterQuestion = window.location.search.toString().substring(1);
+                const [key, value] = afterQuestion.split("=");
 
-        const afterQuestion = window.location.search.substring(1);
-        const [key, value] = afterQuestion.split("=");
-        if (key !== "error") {
-            // get access token and refresh token
-            var authOptions = {
-                url: 'https://accounts.spotify.com/api/token',
-                form: {
-                  code: value,
-                  redirect_uri: REDIRECT_URI,
-                  grant_type: 'authorization_code'
-                },
-                headers: {
-                  'Authorization': 'Basic ' + (Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET, 'base64'))
-                },
-                json: true
-              };
-
-            try {
-                const res = await axios.post(authOptions); 
-                const jsonRes = JSON.parse(res);
-
-                // store Spotify credentials in user object
-                try {
-                    axios.put("/users/" + user._id, {
-                        spotifyAccessToken: jsonRes.access_token,
-                        spotifyRefreshToken: jsonRes.refresh_token
-                    });
-                } catch (err) {
-                    console.log(err);
+                if (key !== "error") {
+                    // get access token and refresh token -- THE ERROR IS WITH THIS POST
+                    var authOptions = {
+                        url: 'https://accounts.spotify.com/api/token',
+                        data: JSON.stringify({
+                            code: value,
+                            redirect_uri: REDIRECT_URI,
+                            grant_type: 'authorization_code'
+                        }),
+                        headers: {
+                        'Authorization': 'Basic ' + (Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET, 'base64')),
+                        'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                        //'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+                        },
+                        json: true
+                    };
+        
+                    try {
+                        const res = await axios.post(authOptions); 
+                        const jsonRes = JSON.parse(res);
+        
+                        // store Spotify credentials in user object
+                        try {
+                            axios.put("/users/" + user._id, {
+                                spotifyAccessToken: jsonRes.access_token,
+                                spotifyRefreshToken: jsonRes.refresh_token
+                            });
+                        } catch (err) {
+                            console.log(err);
+                        }
+                    } catch(err) {
+                        console.log(err);
+                    }
                 }
-            } catch(err) {
-                console.log(err);
+                else {
+                    console.log("error with Spotify authorization")
+                }
+
+                dispatch({ type:"LOGOUT", payload: user })
+                navigate('/login');
             }
         }
-        else {
-            console.log("error with Spotify authorization")
-        }
+        fetchTokens();
+    });
 
+    const handleLogin = () => {
+        // redirect to Spotify authorization page
+        window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES_URL}&response_type=code&show_dialog=true`;
     }
     return ( 
         <div>
