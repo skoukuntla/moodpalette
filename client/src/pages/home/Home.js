a99e067a0d6b18a39913809574d102b0b9ae720f
Aayushi Neema <neema.aayushi@gmail.com>
2023-04-22 18:13:02 -0400

    fixed quotes api

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -15,33 +15,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import partyprimary from '../shop/outfits/party-primary.png'
-import partysecondary from '../shop/outfits/party-secondary.png'
-import crownprimary from '../shop/outfits/crown-primary.png'
-import crownsecondary from '../shop/outfits/crown-secondary.png'
-import cowboyprimary from '../shop/outfits/cowboy-primary.png'
-import cowboysecondary from '../shop/outfits/cowboy-secondary.png'
-import fancyprimary from '../shop/outfits/fancy-primary.png'
-import fancysecondary from '../shop/outfits/fancy-secondary.png'
-import employeeprimary from '../shop/outfits/employee-primary.png'
-import employeesecondary from '../shop/outfits/employee-secondary.png'
-import chefprimary from '../shop/outfits/chef-primary.png'
-import chefsecondary from '../shop/outfits/chef-secondary.png'
-import sportsprimary from '../shop/outfits/sports-primary.png'
-import sportssecondary from '../shop/outfits/sports-secondary.png'
-import ninjaprimary from '../shop/outfits/ninja-primary.png'
-import ninjasecondary from '../shop/outfits/ninja-secondary.png'
-import popstarprimary from '../shop/outfits/popstar-primary.png'
-import popstarsecondary from '../shop/outfits/popstar-secondary.png'
-import discoprimary from '../shop/outfits/disco-primary.png'
-import discosecondary from '../shop/outfits/disco-secondary.png'
-
 export default function Home() {
     const { user } = useContext(AuthContext);
     console.log(user);
-
-    const outfits = [partyprimary, partysecondary, crownprimary, crownsecondary, cowboyprimary, cowboysecondary, fancyprimary, fancysecondary, employeeprimary, employeesecondary, chefprimary, chefsecondary, sportsprimary, sportssecondary, ninjaprimary, ninjasecondary, popstarprimary, popstarsecondary, discoprimary, discosecondary]
-    const mooPalImg = outfits[user.mooPalOutfit]
 
     return (
 
@@ -52,15 +28,13 @@ export default function Home() {
             <br></br>
             <br></br>
             <div className="sprint">
-            { <Calendar></Calendar> }
-            </div>
-          
-            { <GetDailyQuote></GetDailyQuote> }
-            <div className="sprint">
-            {<HabitChecklist></HabitChecklist> }
+                { <Calendar></Calendar> }
             </div>
             <div className="sprint">
-            <img src={mooPalImg} alt="MooPal" width="200"/>
+                { <GetDailyQuote></GetDailyQuote> }
+            </div>
+            <div className="sprint">
+                {<HabitChecklist></HabitChecklist> }
             </div>
             { <SongRecs></SongRecs> }
          </div>
--- client/src/pages/home/quotes/dailyQuote.js
+++ client/src/pages/home/quotes/dailyQuote.js
@@ -1,42 +1,65 @@
-import React, {useState, useEffect} from "react"
-import schedule from 'node-schedule'
+import React, {useState, useContext} from "react"
+import {AuthContext} from "../../../context/AuthContext";
+import axios from "axios";
 import "./dailyQuote.css"
+
+import partyprimary from '../../shop/outfits/party-primary.png'
+import partysecondary from '../../shop/outfits/party-secondary.png'
+import crownprimary from '../../shop/outfits/crown-primary.png'
+import crownsecondary from '../../shop/outfits/crown-secondary.png'
+import cowboyprimary from '../../shop/outfits/cowboy-primary.png'
+import cowboysecondary from '../../shop/outfits/cowboy-secondary.png'
+import fancyprimary from '../../shop/outfits/fancy-primary.png'
+import fancysecondary from '../../shop/outfits/fancy-secondary.png'
+import employeeprimary from '../../shop/outfits/employee-primary.png'
+import employeesecondary from '../../shop/outfits/employee-secondary.png'
+import chefprimary from '../../shop/outfits/chef-primary.png'
+import chefsecondary from '../../shop/outfits/chef-secondary.png'
+import sportsprimary from '../../shop/outfits/sports-primary.png'
+import sportssecondary from '../../shop/outfits/sports-secondary.png'
+import ninjaprimary from '../../shop/outfits/ninja-primary.png'
+import ninjasecondary from '../../shop/outfits/ninja-secondary.png'
+import popstarprimary from '../../shop/outfits/popstar-primary.png'
+import popstarsecondary from '../../shop/outfits/popstar-secondary.png'
+import discoprimary from '../../shop/outfits/disco-primary.png'
+import discosecondary from '../../shop/outfits/disco-secondary.png'
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function GetDailyQuote() {
     const [quote,setQuote] = useState('');
-    const [loading,setLoading] = useState(true);
     const [author,setAuthor]= useState('');
-    const [imgSrc,setImgSrc]= useState('');
 
-    callQuoteAPI();
-    schedule.scheduleJob('0 0 * * *', () => { callQuoteAPI(); }) // run everyday at midnight
+    pullDailyQuote();
 
-    /*useEffect(() => {
-        callQuoteAPI();
-        const intervalID = setInterval(() => {
-            callQuoteAPI();
-        }, 24 * 60 * 60 * 1000);
-        return () => {
-            clearInterval(intervalID);
-        }
-    },[])*/
-
-    function callQuoteAPI() {
-        fetch('http://quotes.rest/qod.json?category=life')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            setQuote(data.contents.quotes[0].quote);
-            setAuthor(data.contents.quotes[0].author);
-            setImgSrc(data.contents.quotes[0].background);
-        })
+    async function pullDailyQuote() {
+        const res = await axios.get("/quote/fetchDailyQuote", {})
+        setQuote(res.data.quote_text)
+        setAuthor(res.data.quote_author)
     }
+
+    const {user} = useContext(AuthContext);
+    const outfits = [partyprimary, partysecondary, crownprimary, crownsecondary, cowboyprimary, cowboysecondary, fancyprimary, fancysecondary, employeeprimary, employeesecondary, chefprimary, chefsecondary, sportsprimary, sportssecondary, ninjaprimary, ninjasecondary, popstarprimary, popstarsecondary, discoprimary, discosecondary]
+    const mooPalImg = outfits[user.mooPalOutfit]
+
     return (
         <div className="quoteDiv">
-            <br />
-            <br />
-            <h1 className="quoteHeader">{quote}</h1>
-            <p className="authorParagraph">- {author}</p>
+            <Container>
+                <Row>
+                    <Col>
+                        <img src={mooPalImg} alt="MooPal" width="200"/>
+                    </Col>
+                    <Col>
+                        <div class="box sb1">
+                            {quote}
+                            <br/><br/>
+                            - {author}
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
