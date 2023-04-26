7c127c660d9dbbc42bd4602b3622718148037bd5
riyaverma11 <verma108@purdue.edu>
2023-04-26 02:40:31 -0400

    priority levels added, cows renamed

--- client/src/pages/habits/HabitTracker.js
+++ client/src/pages/habits/HabitTracker.js
@@ -3,66 +3,80 @@ import NavBar from "../navbar/index";
 import { AuthContext } from "../../context/AuthContext";
 import "./habittracker.css";
 import "reactjs-popup/dist/index.css";
-import axios from "axios";
+import axios, { all } from "axios";
 
 const HabitTracker = () => {
   const { user } = useContext(AuthContext);
-  const [updatedUser, setUser] = useState({user});
-  const allHabits = user.userHabits; 
+  const [updatedUser, setUser] = useState({ user });
+
+  //SINCE WE HAVE LABELED PRIORITIES AS 1,2,3 --> HIGHER PRIORITIES should theoretically BE FIRST ON LIST
+  const allHabits = user.userHabits.sort();
+
   const habit = useRef();
+  const priority = useRef();
 
   const enterHabit = async (e) => {
     e.preventDefault(); // stops page from refreshing on button click
     console.log(habit.current.value);
+    for (let i = 0; i < allHabits.length; i++) {
+      if (allHabits[i] === habit.current.value) {
+        return;
+        //RVTODO: CREATE AN ERROR STATEMENT
+      }
+    }
+
+    const prioritizeHabit = priority.current.value + habit.current.value;
+    console.log(prioritizeHabit);
 
     const addHabit = {
       username: user.username,
-      habit: habit.current.value,
+      habit: prioritizeHabit,
     };
+
+    console.log(priority.current.value);
 
     await axios.post("/users/addHabit", addHabit); // call login api call, asccessing the req.body.habit in users.j
 
     const res = await axios.get(`/users/${user._id}`);
-    
-    console.log("res.data", res.data)
+
+    console.log("res.data", res.data);
     //setUser(res.data);
 
     //localStorage.setItem("user", JSON.stringify(updatedUser.user))
-    localStorage.setItem("user", JSON.stringify(res.data))
+    localStorage.setItem("user", JSON.stringify(res.data));
 
     window.location.reload(false);
     console.log("updated:", updatedUser);
   };
 
-
   const deleteHabit = async (e, habitD) => {
     e.preventDefault(); // stops page from refreshing on button click
-    console.log("habit to be deleted:",habitD.habit);
+    console.log("habit to be deleted:", habitD.habit);
 
     const deleteHabit = {
       username: user.username,
       habit: habitD.habit,
     };
 
-    console.log("deleteHabit1", deleteHabit)
+    console.log("deleteHabit1", deleteHabit);
     const test = await axios.post("/users/deleteHabit", deleteHabit);
-    console.log("test",test)
+    console.log("test", test);
 
     const res = await axios.get(`/users/${user._id}`);
-    
-    console.log("res.data", res.data)
+
+    console.log("res.data", res.data);
     //setUser(res.data);
 
     //localStorage.setItem("user", JSON.stringify(updatedUser.user))
-    localStorage.setItem("user", JSON.stringify(res.data))
+    localStorage.setItem("user", JSON.stringify(res.data));
 
     window.location.reload(false);
     console.log("updated:", updatedUser);
   };
- 
+
   const updateHabit = async (e, habitU) => {
     e.preventDefault(); // stops page from refreshing on button click
-    console.log("habit to be updated:",habitU.habit);
+    console.log("habit to be updated:", habitU.habit);
 
     const updateHabit = {
       username: user.username,
@@ -70,22 +84,26 @@ const HabitTracker = () => {
       newHabit: "something",
     };
 
-    console.log("updateHabit", updateHabit)
+    console.log("updateHabit", updateHabit);
     const test = await axios.put("/users/updateHabit", updateHabit);
-    console.log("test",test)
+    console.log("test", test);
 
     const res = await axios.get(`/users/${user._id}`);
-    
-    console.log("res.data", res.data)
+
+    console.log("res.data", res.data);
     //setUser(res.data);
 
     //localStorage.setItem("user", JSON.stringify(updatedUser.user))
-    localStorage.setItem("user", JSON.stringify(res.data))
+    localStorage.setItem("user", JSON.stringify(res.data));
 
     window.location.reload(false);
     console.log("updated:", updatedUser);
   };
 
+  // const sortHabits = () => {
+  //   allHabits.sort();
+  //   console.log(allHabits)
+  // }
 
   return (
     <div>
@@ -93,13 +111,39 @@ const HabitTracker = () => {
       <br></br>
       <h1 className="header1"> {user.username}'s Habits!!</h1>
       <div>
-      {allHabits?.map((habit, index) => (
-              <div className="listingHabit">
-                <h3>{index + 1} . {habit}</h3>
-                <button className="deleteButton" onClick={(e) => deleteHabit(e, {habit})}>Delete</button>
-                 <button className="updateButton"  onClick={(e) => updateHabit(e, {habit})}>Update</button> 
-              </div>
-      ))}
+        {/* <button onClick={sortHabits}>hi</button> */}
+
+        {allHabits?.map((habit, index) => (
+          <div className="listingHabit">
+            {habit.substring(0, 1) === "1" && (
+              <h3 style={{color:"red"}}>
+                {index + 1} . {habit.substring(1)}
+              </h3>
+            )}
+            {habit.substring(0, 1) === "2" && (
+              <h3 style={{color:"#D29E1E"}}>
+                {index + 1} . {habit.substring(1)}
+              </h3>
+            )}
+            {habit.substring(0, 1) === "3" && (
+              <h3 style={{color:"green"}}>
+                {index + 1} . {habit.substring(1)}
+              </h3>
+            )}
+            <button
+              className="deleteButton"
+              onClick={(e) => deleteHabit(e, { habit })}
+            >
+              Delete
+            </button>
+            <button
+              className="updateButton"
+              onClick={(e) => updateHabit(e, { habit })}
+            >
+              Update
+            </button>
+          </div>
+        ))}
       </div>
 
       <form className="inputHabit" onSubmit={enterHabit}>
@@ -109,14 +153,25 @@ const HabitTracker = () => {
           ref={habit}
         />
 
-        <button className="addHabitButton" type="submit" disabled={allHabits.length >= 10}>
+        <label for="priority">Choose a priority level: </label>
+
+        <select name="priority" id="levels" ref={priority}>
+          <option value="1">High</option>
+          <option value="2">Medium</option>
+          <option value="3">Low</option>
+        </select>
+
+        <button
+          className="addHabitButton"
+          type="submit"
+          disabled={allHabits.length >= 10}
+        >
           Add your habit!
         </button>
         <br></br>
         <br></br>
       </form>
       <div id="passError" style={{ color: "red" }}></div>
-  
     </div>
   );
 };
--- client/src/pages/shop/MooPal.jsx
+++ client/src/pages/shop/MooPal.jsx
@@ -27,7 +27,7 @@ const MooPalShop = () => {
     {<NavBar></NavBar>}
     <br></br>
     <center>
-    <h1>Welcome to the Moo Pal Shop!</h1>  
+    <h1>Welcome to the MooMart!</h1>  
     <br />
     <Grid container spacing={2}>
       <Grid item xs={4}>
@@ -37,31 +37,31 @@ const MooPalShop = () => {
         <OutfitCard outfitName="Party Pal" outfitIndex={0} cost={20} status={setStatus(0)}></OutfitCard>
       </Grid>
       <Grid item xs={4}>
-        <OutfitCard outfitName="Crowned Cow" outfitIndex={2} cost={35} status={setStatus(2)}></OutfitCard>
+        <OutfitCard outfitName="Cow-ntess" outfitIndex={2} cost={35} status={setStatus(2)}></OutfitCard>
       </Grid>
       <Grid item xs={4}>
-        <OutfitCard outfitName="Cowboy Cow" outfitIndex={4} cost={45} status={setStatus(4)}></OutfitCard>
+        <OutfitCard outfitName="Cowcow" outfitIndex={4} cost={45} status={setStatus(4)}></OutfitCard>
       </Grid>
       <Grid item xs={4}>
-        <OutfitCard outfitName="Fancy" outfitIndex={6} cost={50} status={setStatus(6)}></OutfitCard>
+        <OutfitCard outfitName="Moo-nacle" outfitIndex={6} cost={50} status={setStatus(6)}></OutfitCard>
       </Grid>
       <Grid item xs={4}>
-        <OutfitCard outfitName="Employee" outfitIndex={8} cost={65} status={setStatus(8)}></OutfitCard>
+        <OutfitCard outfitName="Cow Worker" outfitIndex={8} cost={65} status={setStatus(8)}></OutfitCard>
       </Grid>
       <Grid item xs={4}>
-        <OutfitCard outfitName="Chef" outfitIndex={10} cost={85} status={setStatus(10)}></OutfitCard>
+        <OutfitCard outfitName="Mooster Chef" outfitIndex={10} cost={85} status={setStatus(10)}></OutfitCard>
       </Grid>
       <Grid item xs={4}>
-        <OutfitCard outfitName="Athlete" outfitIndex={12} cost={110} status={setStatus(12)}></OutfitCard>
+        <OutfitCard outfitName="Kevin Moorant" outfitIndex={12} cost={110} status={setStatus(12)}></OutfitCard>
       </Grid>
       <Grid item xs={4}>
-        <OutfitCard outfitName="Ninja" outfitIndex={14} cost={150} status={setStatus(14)}></OutfitCard>
+        <OutfitCard outfitName="Kung Fu Cow" outfitIndex={14} cost={150} status={setStatus(14)}></OutfitCard>
       </Grid>
       <Grid item xs={4}>
-        <OutfitCard outfitName="Popstar" outfitIndex={16} cost={195} status={setStatus(16)}></OutfitCard>
+        <OutfitCard outfitName="Doja Cow" outfitIndex={16} cost={195} status={setStatus(16)}></OutfitCard>
       </Grid>
       <Grid item xs={4}>
-        <OutfitCard outfitName="Disco" outfitIndex={18} cost={250} status={setStatus(18)}></OutfitCard>
+        <OutfitCard outfitName="Discow" outfitIndex={18} cost={250} status={setStatus(18)}></OutfitCard>
       </Grid>
     </Grid>
     </center>
